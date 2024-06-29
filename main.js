d7353890e8529ca4373fe6c01d0cb35918b3142f
leesuyeon116 <suyeon873088@naver.com>
2024-06-29 12:16:52 +0900

    리셋 버튼해야함

--- main.js
+++ main.js
@@ -1,8 +1,23 @@
 let computerNum = 0;
+let playButton = document.getElementById("play-button");
+let userInput = document.getElementById("user-input");
+let resultArea = document.getElementById("result-area");
+
+playButton.addEventListener("click", play);
 
 function pickRandomNum() {
-    computerNum = Math.floor(Math.random() * 100); // Math.random()함수 -> 0~1사이 숫자를 반환
+    computerNum = Math.floor(Math.random() * 100) + 1; // Math.random()함수 -> 0~1사이 숫자를 반환
     console.log("정답", computerNum);
 }
 
+function play() {
+    let userValue = userInput.value;
+    if(userValue < computerNum) { // 만약에 userValue가 computerNum보다 작으면
+        resultArea.textContent = "UP!";
+    } else if(userValue > computerNum) { // 만약에 userValue가 computerNum보다 크면
+        resultArea.textContent = "DOWN!";
+    } else {
+        resultArea.textContent = "정답입니다~";
+    }
+}
 pickRandomNum();
