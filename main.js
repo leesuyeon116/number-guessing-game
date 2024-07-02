7f39e51f0da9e2f27b3d380dd4501030ddc0c7e0
leesuyeon116 <suyeon873088@naver.com>
2024-07-02 14:37:39 +0900

    정답을 화면에 표시함

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ let userInput = document.querySelector("#user-input");
 let resultText = document.querySelector(".result-text");
 let resultAreaImg = document.querySelector(".main-img");
 let chanceArea = document.getElementById("chance-area");
+let answerNumberText = document.getElementById("answer-number");
+
 let chances = 3; // 남은 기회
 let gameOver = false;
 let userValueList = []; // 유저가 입력한 숫자들 리스트
 
-chanceArea.innerHTML = `남은 기회:${chances}`;
+chanceArea.innerHTML = `남은 기회: ${chances}`;
 playButton.addEventListener("click", play);
 resetButton.addEventListener("click", reset);
 userInput.addEventListener("focus", focusInput);
@@ -17,11 +19,15 @@ userInput.addEventListener("focus", focusInput);
 function pickRandomNumber() {
     computerNumber = Math.floor(Math.random() * 100) + 1; // Math.random()함수 -> 0~1사이 숫자를 반환
     console.log("정답", computerNumber);
+    return computerNumber; // 선택된 정답 반환
 }
+
+// 정답 화면에 표시
+answerNumberText.textContent = pickRandomNumber();
 
 function play() {
     // 숫자 추측하기
-    let userValue = userInput.value;
+    let userValue = parseInt(userInput.value);
     if(userValue < 1 || userValue > 100) { 
         resultText.textContent = "1부터 100까지를 숫자입력";
         return; // 함수 종료
@@ -32,9 +38,9 @@ function play() {
         return;
     }
 
-    chances -- ;
-    chanceArea.textContent = `남은기회:${chances}`; // 텍스트가 아니라 변수 값이 들어옴
+    chances--;
     userValueList.push(userValue);
+
     if(userValue < computerNumber) { // 만약에 userValue가 computerNumber보다 작으면
         resultAreaImg.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExcnB6ODhnMTkwanFzYXpkM3BqeXRvOXljODhjaDUxZHBsZTI5cjdrNSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/xT1R9Ie9mRoRs0w3C0/giphy.gif";
         resultText.textContent = "UP!";
@@ -46,17 +52,22 @@ function play() {
     } else {
         resultAreaImg.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExenMwNGR3d2Y2Y2EweXZncmZucnA2cmdyaDVtdXE0aGZzdWptaDV3ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YFis3URdQJ6qA/giphy.gif"
         resultText.textContent = "와우~정답!";
+        gameOver = true;
+    }
+
+    chanceArea.textContent = `남은기회:${chances}`; 
+
+    if(gameOver) {
+        playButton.disabled = true;
+        resultAreaImg.src = "https://i.giphy.com/media/v1.Y2lkPTc5MGI3NjExenMwNGR3d2Y2Y2EweXZncmZucnA2cmdyaDVtdXE0aGZzdWptaDV3ciZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/YFis3URdQJ6qA/giphy.gif";
         resultText.textContent = "정답입니다~";
-        gameOver = true;
     }
 
-    if(chances < 1) {
+    if(chances === 0) {
         gameOver = true;
+        answerNumberText.textContent = computerNumber;
         resultAreaImg.src = "https://image.kmib.co.kr/online_image/2018/1126/611819110012870035_3.jpg";
-        resultText.textContent = "끝";
-    }
-
-    if(gameOver == true) {
+        resultText.textContent = `끝! 정답은 ${computerNumber}였습니다.`;
         playButton.disabled = true;
     }
 }
@@ -64,7 +75,6 @@ function play() {
 function focusInput() {
     userInput.value = "";
 }
-
 
 function reset() {
     // 리셋
@@ -77,6 +87,7 @@ function reset() {
     chances = 3;
     chanceArea.innerHTML = `남은 기회:${chances}`;
     userValueList = [];
+    answerNumberText.textContent = computerNumber; // 초기화 시 정답을 화면에 표시
 }
 
 pickRandomNumber();
